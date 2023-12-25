a731eb0b33b2b639316b868da31546ffa13f41b8
DEVESH KUMAR SINGH BAGHEL <89765525+Iboys01@users.noreply.github.com>
2023-12-25 15:54:20 +0530

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,44 @@
+// console.log("Hey Devesh I Love You ")
+// console.log("Hey Devesh I Love You ")
+// console.log("Hey Devesh I Love You ")
+// console.log("Hey Devesh I Love You ")
+// console.log("Hey Devesh I Love You ")
+
+
+
+// function nice(name) {
+//     console.log("Hey "+ name+ " you are nice")
+//     console.log("Hey "+ name+ " you are nice")
+//     console.log("Hey "+ name+ " you are nice")
+//     console.log("Hey "+ name+ " you are nice")
+    
+// }
+// nice("Amit")
+
+// nice("Devesh")
+
+
+// function sum(a,b ,c=7){
+//  console.log(a +b)
+// return a+b +c
+// }
+
+//  result1 = sum(3,5)
+//  result2 = sum(5,5)
+//  result3 = sum(7,5,1)
+
+//  console.log("The Sum of thrse Number is: ", result1 , result2,result3)
+
+
+
+
+ const func1 =(x)=>{
+    console.log("I am an arrow function",x)
+
+
+ }
+
+ func1(84);
+ func1(44);
+ func1(94);
+ 
