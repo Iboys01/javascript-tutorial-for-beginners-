7510675d3f712596789ed6d4123dda127adff946
DEVESH KUMAR SINGH BAGHEL <89765525+Iboys01@users.noreply.github.com>
2023-12-25 15:33:11 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,50 @@
+
+// Basic Loops
+
+console.log(" javascript tutorial for beginners ")
+
+// let a=1;
+
+
+// for(let i= 70; i <100; i++){
+//     console.log(a+i);
+// } 
+
+
+// for loops
+
+// let obj ={
+//     name: "Devesh",
+//     role: "Programmer",
+//     company: "CodeWithHarry AI "
+// }
+// for (const key in obj){
+
+//     console.log(key)
+// }
+
+
+
+// for (const d of devesh) {
+//     console.log(c)
+    
+// }
+
+
+
+
+
+
+
+
+// let i = 5;
+// while (i<600){
+// console.log(i)
+// i++
+// }
+
+let i =0;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
